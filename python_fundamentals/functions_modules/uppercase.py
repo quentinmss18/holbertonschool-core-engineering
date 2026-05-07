1def02cb2f4ee4b67acfc0a8e7912ea103a86750
e <quentinmoussous@gmail.com>
2026-05-07 13:12:46 +0200

    upper

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -2,19 +2,13 @@
 
 def uppercase(str):
     """
-    Parcourt la chaîne et convertit chaque minuscule en majuscule
-    en manipulant les valeurs ASCII avec ord() et chr().
+    Prints a string in uppercase followed by a new line.
     """
     result = ""
     for char in str:
-        # On vérifie si le caractère est une minuscule
         if ord(char) >= 97 and ord(char) <= 122:
-            # On soustrait 32 pour obtenir la majuscule, 
-            # puis on transforme le nombre en caractère avec chr()
             result += chr(ord(char) - 32)
         else:
-            # Si ce n'est pas une minuscule, on garde le caractère tel quel
             result += char
-
-    # On imprime le )
-~                                                           
+    print("{}".format(result))
+~            
