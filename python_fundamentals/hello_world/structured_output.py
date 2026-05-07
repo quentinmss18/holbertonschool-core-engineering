1e7f7216211f1260be0b1e5845f3a5e1b06427e1
ben <quentinmoussous@gmail.com>
2026-05-07 11:42:14 +0200

    python first commit

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+language = "Python"
+version = 3
+pi = 3.14159
+is_valid = pi > 3.0
+
+print(f"Language: {language}")
+print("Version:", version)
+print("Pi approx: {:.2f}".format(pi))
+print("Computation valid:", is_valid)
