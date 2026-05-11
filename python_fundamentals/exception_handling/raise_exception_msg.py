55a479db90a31519c969f5736d57b139c3d45d19
ben <quentinmoussous@gmail.com>
2026-05-11 09:47:17 +0000

    raise exception

---
+++ python_fundamentals/exception_handling/raise_exception_msg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def raise_exception_msg(message=""):
+    """
+    Raises a NameError with a specific message.
+
+    Args:
+        message: The string message to include in the exception.
+    """
+    raise NameError(message)
